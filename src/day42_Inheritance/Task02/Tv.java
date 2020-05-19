1504ad05bc712924fef7ab9e8863b9feae76576c
Mabouyas <Cybertek.QA@gmail.com>
2020-05-19 12:26:41 -0400

    Initial commit

--- src/day42_Inheritance/Task02/Tv.java
+++ src/day42_Inheritance/Task02/Tv.java
@@ -24,7 +24,7 @@ create a class called TV:
 		setDevice(brand,model, price, "TV", size );
 	}
 	
-	public void wacth(){
+	public void watch(){
 		
 		System.out.println("I am watching "+brand+" "+model+" "+type+" that cost me $"+price+" from "+country );
 		
